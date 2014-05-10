ec6bdc150c7f480be4209a04b94c2e6eaca5613f
Chris Goldsberry <christopher.goldsberry@gmail.com>
2014-05-09 23:03:39 -0500

    fixed active record railtie by commenting out seventh line so RSPEC would work.  Updated Readme

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,25 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '8e93fce0f07363c91f56dc2d27c4271b1c2eebaa07a4833b0d39cdbbd8ee1fe63c83dbc38ae76ba37f85b4f9a4b1498fafe0d94b2abb9064c7595a0e308104be'
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# use the existing token
+		File.read(token_file).chomp
+	else
+		# Generate a new token and store it in the token file
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+
+
+SampleApp::Application.config.secret_key_base = secure_token
